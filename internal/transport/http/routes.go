15cd16ae6736a1e9da4b4196b16a27f58e5f6e80
rizheez <rezamuhammad980@gmail.com>
2025-08-21 12:49:45 +0800

    Add user and category endpoints to HTTP routes for retrieval and creation

--- internal/transport/http/routes.go
+++ internal/transport/http/routes.go
@@ -5,4 +5,8 @@ import "github.com/gofiber/fiber/v2"
 func SetupRoutes(app *fiber.App) {
 	app.Get("/posts", GetPosts)
 	app.Post("/posts", CreatePost)
+	app.Get("/users", GetAllUsers)
+	app.Post("/users", CreateUser)
+	app.Get("/categories", GetAllCategories)
+	app.Post("/categories", CreateCategory)
 }
