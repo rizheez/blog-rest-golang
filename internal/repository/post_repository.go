af982d42b3a6ad58c1faec82905e518cbd7de135
rizheez <rezamuhammad980@gmail.com>
2025-08-22 16:25:55 +0800

    feat✨ (post_repository.go): add functions to get, update, and delete posts from the database

--- internal/repository/post_repository.go
+++ internal/repository/post_repository.go
@@ -12,6 +12,22 @@ func GetAllPosts() ([]models.Post, error) {
 	return posts, err
 }
 
+func GetPostById(id int) (models.Post, error) {
+	post := models.Post{}
+	result := database.DB.Preload("User").Preload("Category").First(&post, id)
+	err := result.Error
+	return post, err
+}
+
 func CreatePost(post *models.Post) error {
 	return database.DB.Create(post).Error
+}
+
+func UpdatePost(post *models.Post) error {
+	return database.DB.Model(&models.Post{}).Where("id = ?", post.ID).Updates(post).Error
+
+}
+
+func DeletePost(post *models.Post) error {
+	return database.DB.Delete(post).Error
 }
