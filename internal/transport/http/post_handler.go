ac1d0790c437d18502db2e7549fd97d2a69816f5
rizheez <rezamuhammad980@gmail.com>
2025-08-22 16:27:38 +0800

    🐛 (routes.go): add routes for get, put, delete post by id

--- internal/transport/http/post_handler.go
+++ internal/transport/http/post_handler.go
@@ -3,6 +3,7 @@ package http
 import (
 	"blog-rest/internal/models"
 	"blog-rest/internal/services"
+	"strconv"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -17,6 +18,24 @@ func GetPosts(c *fiber.Ctx) error {
 	}
 
 	return c.JSON(posts)
+}
+
+func GetPostById(c *fiber.Ctx) error {
+	id, err := c.ParamsInt("id")
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Invalid id",
+			"code":  fiber.StatusBadRequest,
+		})
+	}
+	post, err := services.GetPostById(id)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": err.Error(),
+			"code":  fiber.StatusInternalServerError,
+		})
+	}
+	return c.JSON(post)
 }
 
 func CreatePost(c *fiber.Ctx) error {
@@ -36,4 +55,51 @@ func CreatePost(c *fiber.Ctx) error {
 
 	return c.JSON(post)
 
+}
+
+func UpdatePost(c *fiber.Ctx) error {
+	idParams := c.Params("id")
+	id, err := strconv.Atoi(idParams)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Invalid id",
+			"code":  fiber.StatusBadRequest,
+		})
+	}
+	var post models.Post
+	if err := c.BodyParser(&post); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": err.Error(),
+			"code":  fiber.StatusInternalServerError,
+		})
+	}
+	post.ID = uint(id)
+	if err := services.UpdatePost(&post); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": err.Error(),
+			"code":  fiber.StatusInternalServerError,
+		})
+	}
+	return c.JSON(post)
+}
+
+func DeletePost(c *fiber.Ctx) error {
+	idParams := c.Params("id")
+	id, err := strconv.Atoi(idParams)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Invalid id",
+			"code":  fiber.StatusBadRequest,
+		})
+	}
+	var post models.Post
+	post.ID = uint(id)
+	err = services.DeletePost(&post)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": err.Error(),
+			"code":  fiber.StatusInternalServerError,
+		})
+	}
+	return c.SendStatus(fiber.StatusOK)
 }
